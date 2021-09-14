f542c4ecc58f951c0703a00a01ace0a41c9d63cd
justxyx <635893003@qq.com>
2021-09-14 17:40:17 +0800

    外观模式

---
+++ src/design_pattern/外观模式/p1外观模式的实现/Projector.java
@@ -0,0 +1,16 @@
+package design_pattern.外观模式.p1外观模式的实现;
+
+/**
+ * @author xm
+ * @date 2021/9/14 17:33
+ * @description:
+ */
+public class Projector {
+    void open(){
+        System.out.println("打开投影仪");
+    }
+    void closed(){
+        System.out.println("关闭投影仪");
+    }
+}
+
